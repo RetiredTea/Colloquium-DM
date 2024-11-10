6ffa9837ea08ecfcfb1995a0d61c3cbef0a35a9e
Царегородцев Денис <144447515+denisayrus@users.noreply.github.com>
2024-11-10 18:07:44 +0300

    with_functions_for_polynoms

--- my_functions_for_colloc.py
+++ my_functions_for_colloc.py
@@ -5,8 +5,8 @@ from YoptaCode import * # код Артема
 #===== принимает два числа возвращает одно - НОД (ввод/вывод объектами класса NaturalNumber) ====
 def GCF_NN_N(num1: NaturalNumber, num2: NaturalNumber):
     def swap(a: NaturalNumber, b: NaturalNumber): 
-        if a > b:
-            return a, b # нужна функция сравнения пока что простая затычка
+        if COM_NN_D(a,b) == 2:
+            return a, b 
         c = a
         a = b
         b = c
@@ -20,14 +20,15 @@ def GCF_NN_N(num1: NaturalNumber, num2: NaturalNumber):
 #===== НОК натуральных чисел ====
 #===== принимает два числа возвращает одно - НОК (ввод/вывод объектами класса NaturalNumber) ====
 def LCM_NN_N(num1: NaturalNumber, num2: NaturalNumber):
-    return MOD_NN_N(num1 * num2, GCF_NN_N(num1, num2)) # нужна функция умножения чисел
+    return MOD_NN_N(MULL_NN_N(num1,num2), GCF_NN_N(num1, num2)) # нужна функция умножения чисел
 
 #===== Сложение дробей ====
 #===== принимает две дроби возвращает их сумму(ввод/вывод рациональое число) ====
 # Нужны функции сложения целых чисел, умножения целового на целое, преобразования натурального в целое
 def ADD_QQ_Q (frac1: RationalNumber, frac2: RationalNumber):
     denominator = (LCM_NN_N(frac1.denominator,frac2.denominator))
-    numerator = frac1.numerator * (MOD_NN_N(denominator, frac1.denominator)) + frac2.numerator * (MOD_NN_N(denominator, frac2.denominator))
+    numerator = ADD_ZZ_Z(MUL_ZZ_Z(frac1.numerator, TRANS_N_Z((MOD_NN_N(denominator, frac1.denominator)))),\
+                        MUL_ZZ_Z(frac2.numerator, TRANS_N_Z(MOD_NN_N(denominator, frac2.denominator))))
     frac_sum = RationalNumber(numerator, denominator)
     return(frac_sum) 
 
@@ -36,26 +37,85 @@ def ADD_QQ_Q (frac1: RationalNumber, frac2: RationalNumber):
 # нужны функции умножения целых чисел, вычетания целых чисел, преобразования натурального в целое
 def SUB_QQ_Q(frac1: RationalNumber, frac2: RationalNumber):
     denominator = (LCM_NN_N(frac1.denominator, frac2.denominator))
-    numerator = frac1.numerator * (MOD_NN_N(denominator, frac1.denominator)) - frac2.numerator * (MOD_NN_N(denominator, frac2.denominator))
+    numerator = SUB_ZZ_Z(MUL_ZZ_Z(frac1.numerator, TRANS_N_Z((MOD_NN_N(denominator, frac1.denominator)))),\
+                        MUL_ZZ_Z(frac2.numerator, TRANS_N_Z(MOD_NN_N(denominator, frac2.denominator))))
     frac_sum = RationalNumber(numerator, denominator)
     return(frac_sum)
 
 #===== Сложение многочленов ====
 #=====  ====
-def P1(polyn1:str, polyn2:str):
-    pass
+def ADD_PP_P(polyn1: Polynomial, polyn2: Polynomial):
+    result = Polynomial()  # Создаем новый многочлен для результата
+    # Сначала добавляем все члены из первого многочлена
+    temp = polyn1.head
+    while temp is not None:
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    # Затем добавляем все члены из второго многочлена
+    temp = polyn2.head
+    while temp is not None:
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    return result
 
 #===== Вычитание многочленов ====
 #=====  ====
-def P2():
-    pass
+def SUB_PP_P(polyn1: Polynomial, polyn2: Polynomial):
+    result = Polynomial()  # Создаем новый многочлен для результата
+    # Сначала добавляем все члены из первого многочлена
+    temp = polyn1.head
+    while temp is not None:
+        temp.val.numerator = MUL_ZM_Z(temp.val.numerator)
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    # Затем вычитаем все члены из второго многочлена
+    temp = polyn2.head
+    while temp is not None:
+        temp.val.numerator = MUL_ZM_Z(temp.val.numerator)
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    return result
 
 #===== НОД многочленов ====
 #=====  ====
-def P11():
-    pass
+def GCF_PP_P(polyn1: Polynomial, polyn2: Polynomial):
+    result = Polynomial()
+    while polyn1.head is not None and polyn2.head is not None:
+        if int(str(polyn1.head.deg)) > int(str(polyn2.head.deg)):
+            polyn1 = MOD_PP_P(polyn1,polyn2)
+        elif int(str(polyn1.head.deg)) == int(str(polyn2.head.deg)) and \
+           int(str((SUB_QQ_Q(polyn1.head.val, polyn2.head.val)).numerator)) > 0:
+            polyn1 = MOD_PP_P(polyn1,polyn2)
+        elif int(str(polyn1.head.deg)) == int(str(polyn2.head.deg)) and \
+           int(str((SUB_QQ_Q(polyn1.head.val, polyn2.head.val)).numerator)) < 0:
+            polyn2 = MOD_PP_P(polyn2,polyn1)
+        else:
+            polyn2 = MOD_PP_P(polyn2,polyn1)
+    temp = polyn1.head
+    while temp is not None:
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    # Затем добавляем все члены из второго многочлена
+    temp = polyn2.head
+    while temp is not None:
+        result.add(temp.deg, temp.val)
+        temp = temp.next
+    return result
 
 #===== Производная многочлена ====
 #=====  ====
-def P12():
-    pass
+def DER_P_P(polyn: Polynomial):
+    temp = polyn.head
+    while temp is not None:
+        if str(temp.deg) == '1':
+            temp.deg = '0'
+
+        if str(temp.deg) == '0':
+            temp = temp.prev
+            temp.next = None
+            break
+        if str(temp.deg) != '1' and str(temp.deg) != '0':
+            temp.val = MUL_QQ_Q(temp.val, TRANS_Z_Q(TRANS_N_Z(temp.deg)))
+            temp.deg = SUB_NN_N(temp.deg, NaturalNumber('1'))
+        temp = temp.next
+    return polyn
