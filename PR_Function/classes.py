c391b0fb63df05eb9e778718121e51f1997a85f7
Александр Ширинкин <alex.shirinkinkin@bk.ru>
2024-11-13 18:11:11 +0300

    Our_win.exe

---
+++ PR_Function/classes.py
@@ -0,0 +1,431 @@
+import re
+
+
+# Класс целого числа
+# Хранится в аналогично натуральному, но присутствует знак в виде int.
+class IntegerNumber:
+
+    def __init__(self, value: str):
+
+        # Проверки на корректность ввода
+        if not isinstance(value, str):
+            raise ValueError("Число должно быть представлено строкой.")
+        elif not (value.lstrip('-').isdigit()):
+            raise ValueError("Значение должно быть целым числом.")
+
+        # Знак числа хранится как int. (0 - "+", 1 - "-")
+        self.sign = 0
+
+        # Инициализация отрицательного и положительно числа соответственно
+        if value[0] == '-':
+            self.sign = 1
+            self.value = list(map(int, list(value.lstrip('-'))))
+            if self.value == [0]:
+                self.sign = 0
+        else:
+            self.value = list(map(int, list(value)))
+
+
+    # Вывод числа в виде строки
+    def __str__(self):
+        if self.sign == 1:
+            return f"-{''.join(list(map(str, list(self.value))))}"
+        else:
+            return ''.join(list(map(str, list(self.value))))
+
+    # Вывод числа в виде цечисленного значения
+    def __int__(self):
+        return int(str(self))
+
+    # Возврат массива цифр
+    def get_value(self):
+        return self.value
+
+    # Возврат знака
+    def get_sign(self):
+        return self.sign
+
+    # Перегруженный len
+    def __len__(self):
+        return len(self.value)
+
+
+# Класс натурального числа
+# Хранение натурального числа подразумевается в виде массива чисел ->
+# Это поможет реализации посимвольных математических операций.
+class NaturalNumber:
+
+    def __init__(self, value: str):
+
+        # Проверки на корректность ввода
+        if not isinstance(value, str):
+            raise ValueError('Число должно быть представлено строкой.')
+        elif not value.isdigit() or int(value) < 0:
+            raise ValueError('Значение должно быть натуральным числом или нулём.')
+
+        # Инициализация массивом цифр
+        self.value = list(map(int, list(str(int(value)))))
+
+    # Вывод числа в виде строки
+    def __str__(self):
+        return ''.join(list(map(str, list(self.value))))
+
+    # Вывод числа в виде цечисленного значения
+    def __int__(self):
+        return int(str(self))
+
+    # Возврат массива цифр
+    def get_value(self):
+        return self.value
+
+    # Перегруженный len
+    def __len__(self):
+        return len(self.value)
+
+
+# Класс рационального числа
+# Хранится как целое и натуральное число для числителя и знаменателя соответственно.
+class RationalNumber:
+
+    def __init__(self, numerator, denominator=NaturalNumber("1")):
+
+        # Проверки на корректность ввода
+        if (not isinstance(numerator, IntegerNumber)) and (not isinstance(denominator, NaturalNumber)):
+            raise ValueError(
+                "Числитель и знаменатель должны быть классами IntegerNumber и NaturalNumber соответственно.")
+
+        # Инициализация числителя и знаменателя
+        self.numerator = numerator
+        if denominator.__str__() != "0":
+            self.denominator = denominator
+        else:
+            raise ValueError("Знаменатель не может быть нулём")
+
+    # Вывод числа в виде строки
+    def __str__(self):
+
+        # Так как числитель и знаменатель - объекты предыдущих классов, вызываются методы __str__.
+        return f"{str(self.numerator)}/{str(self.denominator)}"
+
+
+# Вспомогательный класс узла многочлена
+# Поле val - коэффициент
+# Поле deg - степень
+class PolynomialNode:
+    def __init__(self, deg=NaturalNumber("0"), val=RationalNumber("0"), prev=None, next=None):
+        if (not isinstance(deg, NaturalNumber)) and (not isinstance(val, RationalNumber)):
+            raise ValueError(
+                "Степень и коэффициент должны быть классами NaturalNumber и RationalNumber соответственно.")
+        self.val = val
+        self.deg = deg
+        self.prev = prev
+        self.next = next
+
+
+# Класс многочлена
+# Реализован на базе двусвязного списка
+class Polynomial:
+    # Инициализация многочлена
+    def __init__(self):
+        self.head = PolynomialNode()
+
+    # Возвращает многочлен в виде строки
+    def __str__(self):
+        res = ""
+        temp = self.head
+        while temp is not None:
+            if int(temp.val.numerator) != 0:
+                if res:  # Если это не первый элемент
+                    if (temp.val.numerator.get_sign() == 0):
+                        res += " + "
+                    else:
+                        res += " - "
+                else:
+                    if (temp.val.numerator.get_sign() == 1):
+                        res += "-"
+
+                coeff_str = ""
+                if (int(temp.val.denominator) == 1 and abs(int(temp.val.numerator)) != 1):
+                    coeff_str = str(temp.val.numerator)
+                elif (int(temp.val.denominator) != 1):
+                    coeff_str = str(temp.val)
+                if (temp.val.numerator.get_sign() == 1):
+                    coeff_str = coeff_str[1:]
+                if int(temp.deg) == 0:
+                    if coeff_str == "":
+                        coeff_str = "1"
+                    res += coeff_str
+                else:
+                    if int(temp.deg) == 1:
+                        res += f"{coeff_str}x"
+                    else:
+                        res += f"{coeff_str}x^{str(temp.deg)}"
+            temp = temp.next
+
+        return res or "0"
+
+    # Зануляет все коэффициенты
+    def clear(self):
+        temp = self.head
+        while (temp != None):
+            temp.val = RationalNumber("0")
+            temp = temp.next
+
+    # Добавление/изменение (сумма старого и нового коэффициентов) одночлена
+    def add(self, deg: NaturalNumber, val: RationalNumber):
+        if (not isinstance(deg, NaturalNumber)) and (not isinstance(val, RationalNumber)):
+            raise ValueError(
+                "Степень и коэффициент должны быть классами NaturalNumber и RationalNumber соответственно.")
+
+        temp = self.head
+        while (temp != None):
+            # Ищем для вставки место в списке
+            if (int(deg) < int(temp.deg) and temp.next != None):
+                temp = temp.next
+
+            # Если у степени уже есть коэффициент
+            elif (str(deg) == str(temp.deg)):
+                # Сложение рациональных чисел:
+                # (a/b) + (c/d) = (a*d + b*c) / (b*d)
+                # Получаем числители и знаменатели
+                a = int(temp.val.numerator)
+                b = int(temp.val.denominator)
+                c = int(val.numerator)
+                d = int(val.denominator)
+
+                # Вычисляем новый числитель и знаменатель
+                new_numerator_value = IntegerNumber(str(a * d + b * c))
+                new_denominator_value = NaturalNumber(str(b * d))
+                temp.val = RationalNumber(new_numerator_value, new_denominator_value)
+                break
+
+            # Если нашли место для вставки, то вставляем
+            else:
+                elem = PolynomialNode(deg, val)
+                if (int(deg) > int(temp.deg)):
+                    elem.next = temp
+                    elem.prev = temp.prev
+                    if temp.prev != None:
+                        temp.prev.next = elem
+                    temp.prev = elem
+                else:
+                    elem.prev = temp
+                    elem.next = temp.next
+                    if temp.next != None:
+                        temp.next.prev = elem
+                    temp.next = elem
+                break
+        temp = self.head.prev
+
+        # Если после вставки элемента мы не в начале то переходим в начало
+        while (temp != None):
+            self.head = temp
+            temp = temp.prev
+
+    # Создание многочлена при помощи входной строки
+    def makePolynomial(self, input_str: str):
+        # Удаляем пробелы и разбиваем строку на термины
+        input_str = input_str.replace(" ", "")
+        input_str = input_str.replace("*", "")
+        terms = re.split(r'(?=[+-])', input_str)  # Разделяем по знакам
+
+        for term in terms:
+            if term:  # Проверяем, что строка не пустая
+                term = term.replace("+", "")
+                if 'x' in term:
+                    # Обрабатываем термины с x
+                    if '^' in term:
+                        coeff_str, deg = term.split('x^')
+                    else:
+                        coeff_str = term[:-1]  # Убираем 'x'
+                        deg = "1"
+
+                    if coeff_str == "-" or coeff_str == "":
+                        coeff_str += "1"
+                    if '/' in coeff_str:
+                        num_str, denom_str = coeff_str.split('/')
+                        coeff = RationalNumber(IntegerNumber(num_str), NaturalNumber(denom_str))
+                    else:
+                        coeff = RationalNumber(IntegerNumber(coeff_str))
+
+
+                else:
+                    # Обрабатываем константы (термины без x)
+                    if '/' in term:
+                        num_str, denom_str = term.split('/')
+                        coeff = RationalNumber(IntegerNumber(num_str), NaturalNumber(denom_str))
+                    else:
+                        coeff = RationalNumber(IntegerNumber(term))
+                    deg = "0"
+                # Добавляем в многочлен
+                self.add(NaturalNumber(deg), coeff)
+
+    # Возвращает коэффициент при степени
+    def getCoeff(self, deg: NaturalNumber):
+        if not isinstance(deg, NaturalNumber):
+            raise ValueError("Степень должна быть классом NaturalNumber.")
+
+        # Проходимся по списку и возвращаем значение
+        temp = self.head
+        while (temp != None):
+            if (int(temp.deg) == int(deg)):
+                return temp.val
+            temp = temp.next
+        return RationalNumber("0")
+
+    # Возвращает массив степеней, у которых коэффициенты отличны от нуля
+    def getDegrees(self):
+        arr = []
+        # Проходимся по списку и добавляем в массив степень, если коэффициент не ноль
+        temp = self.head
+        while (temp != None):
+            if int(temp.val.numerator) != 0:
+                arr.append(int(temp.deg))
+            temp = temp.next
+        return arr or [0]
+
+    # Изменяет степень на новую
+    def changeDegree(self, deg: NaturalNumber, newdeg: NaturalNumber):
+        if (not isinstance(deg, NaturalNumber)) and (not isinstance(newdeg, NaturalNumber)):
+            raise ValueError("Степени должны быть классом NaturalNumber.")
+
+        # Проходимся по списку
+        temp = self.head
+        while (temp != None):
+            # Если нашли значение, то добавляем новый элемент с новой степенью и старым коэффициентом, а коэффициент у старой степени зануляем
+            if int(temp.deg) == int(deg):
+                self.add(newdeg, temp.val)
+                temp.val = 0
+            temp = temp.next
+        return
+
+    # Возвращает красивую строку (многочлен), использовать только для конечного вывода
+    def getNiceStr(self):
+        def func_q1(rational_number: RationalNumber) -> RationalNumber:
+            def euc_alg(a, b):
+                # Функция меняющая между собой значения 2 переданных переменных
+                def swap(a, b):
+                    return b, a
+
+                # Коэффициенты алгоритма Евклида
+                coeffs = []
+
+                # Применяем алгоритм Евклида
+                while (b != 0):
+                    coeffs.append(a // b)
+                    a = a % b
+                    a, b = swap(a, b)
+
+                # Записываем результаты алгоритма Евклида (nod)
+                return a
+
+            if (rational_number.denominator == 1):
+                return rational_number
+
+            nod = euc_alg(int(rational_number.numerator), int(rational_number.denominator))
+            rational_number.numerator = IntegerNumber(str(int(rational_number.numerator) // nod))
+            rational_number.denominator = NaturalNumber(str(int(rational_number.denominator) // nod))
+
+            return rational_number
+
+        indexes = {"0": "\u2070",
+                   "1": "\u00B9",
+                   "2": "\u00B2",
+                   "3": "\u00B3",
+                   "4": "\u2074",
+                   "5": "\u2075",
+                   "6": "\u2076",
+                   "7": "\u2077",
+                   "8": "\u2078",
+                   "9": "\u2079",
+                   "-": "\u207B"
+                   }
+
+        temp = self.head
+        while temp is not None:
+            if int(temp.val.numerator) != 0:
+                temp.val = func_q1(temp.val)
+            temp = temp.next
+
+        def degree(deg):
+            degree = ""
+            temp = str(deg)
+            for char in temp:
+                degree += indexes[char] or ""
+            return degree
+
+        if not self.head:
+            return "0"
+
+        res = ""
+        temp = self.head
+        while temp is not None:
+            if int(temp.val.numerator) != 0:
+                if res:  # Если это не первый элемент
+                    if (temp.val.numerator.get_sign() == 0):
+                        res += " + "
+                    else:
+                        res += " - "
+                else:
+                    if (temp.val.numerator.get_sign() == 1):
+                        res += "-"
+
+                coeff_str = ""
+                if (int(temp.val.denominator) == 1 and abs(int(temp.val.numerator)) != 1):
+                    coeff_str = str(temp.val.numerator)
+                elif (int(temp.val.denominator) != 1):
+                    coeff_str = str(temp.val)
+                if (temp.val.numerator.get_sign() == 1):
+                    coeff_str = coeff_str[1:]
+                if int(temp.deg) == 0:
+                    if coeff_str == "":
+                        coeff_str = "1"
+                    res += coeff_str
+                else:
+                    if int(temp.deg) == 1:
+                        res += f"{coeff_str}x"
+                    else:
+                        res += f"{coeff_str}x{degree(temp.deg)}"
+            temp = temp.next
+
+        return res or "0"
+
+    def reduceCoeffs(self):
+        def find_nod(a, b):
+            def swap(a, b):
+                return b, a
+
+            while (b != 0):
+                a = a % b
+                a, b = swap(a, b)
+            # Записываем результаты алгоритма Евклида (nod)
+            return a
+
+        def GCF_ARR(arr):
+            if (len(arr) == 0):
+                return 1
+            nod = arr[0]
+            for i in range(1, len(arr)):
+                nod = find_nod(nod, arr[i])
+            return nod
+
+        coeffs = []
+        for i in self.getDegrees():
+            coef = self.getCoeff(NaturalNumber(str(i)))
+            nod = find_nod(abs(int(coef.numerator)), int(coef.denominator))
+            if (int(coef.denominator) // nod != 1):
+                return
+            coeffs.append(int(coef.numerator) // nod)
+        gcf = GCF_ARR(coeffs)
+        if (gcf > 1):
+            temp = self.head
+            while temp != None:
+                temp.val.numerator = IntegerNumber(str(int(temp.val.numerator) // gcf))
+                temp = temp.next
+        return
+
+
+def makePolynomial(input_str: str) -> Polynomial:
+    pln = Polynomial()
+    pln.makePolynomial(input_str)
+    return pln
---
+++ PR_Function/section_template.py
@@ -0,0 +1,247 @@
+import tkinter as tk
+from functions import functions_dict
+from classes import *
+
+class SectionFrame(tk.Frame):
+    def __init__(self, parent, func_number, func_name, result_label):
+        super().__init__(parent)
+
+        # Создаем основной контейнер для размещения лейблов и полей ввода
+        main_container = tk.Frame(self)
+        main_container.pack(fill="both")
+
+        # Левый фрейм для отображения доступных операций
+        operations_frame = tk.Frame(main_container)
+        operations_frame.pack(side="left", padx=10, pady=10, anchor="n")
+
+        operations_label = tk.Label(operations_frame, text="Доступные операции:", font=("Arial", 10, "bold"))
+        operations_label.pack(anchor="w")
+
+        operations = [
+            "+  - сложение",
+            "-  - вычитание",
+            "*  - умножение",
+            ":  - деление",
+            "^  - возведение в степень",
+            "%  - остаток",
+            "?  - сравнение",
+            """　　　　　　　　 　　　 ／ ¯¯｀フ
+　　　　　　　　　,　'' ｀ ｀ / 　 　 　 !　 　
+　　　　　　　 , ' 　　　　 レ　 _,　 -' ミ
+　　　　　　　 ; 　 　 　 　 　`ミ __,xノﾞ､
+　　　 　　 　 i　 　　　ﾐ　　　; ,､､､、　ヽ ¸
+　　　 　　,.-‐! 　 　 　 ﾐ　　i　　　　｀ヽ.._,,))
+　　 　　//´｀｀､　　　　 ミ　ヽ　　　　　(¯`v´¯)
+　　　　| l　　 　｀ ｰｰ -‐''ゝ､,,)).　 　　 　 ..`·.¸.·´
+　　　 　ヽ.ー─'´)　"""
+        ]
+
+        for operation in operations:
+            op_label = tk.Label(operations_frame, text=operation, anchor="w")
+            op_label.pack(anchor="w")
+
+        # Центральный фрейм для ввода и вывода, который будет центрироваться в оставшемся пространстве
+        central_frame = tk.Frame(main_container)
+        central_frame.pack(side="left", expand=True, fill="both", padx=10, pady=10)
+
+        # Центрируем содержимое central_frame по середине
+        content_frame = tk.Frame(central_frame)
+        content_frame.pack(expand=True)  # Делаем контентный фрейм центрированным
+
+        # Лейбл для отображения названия функции
+        self.label = tk.Label(content_frame, text=func_name, font=("Arial", 14), justify="center")
+        self.label.pack(pady=(0, 10))
+
+        # Определение категорий функций
+        self.single_arg_funcs = ["2", "3", "15", "16", "17"
+                                ,"18", "19", "25", "26", "27", "28"
+                                ,"37", "38", "39", "44", "45"]
+        self.dual_arg_funcs = {
+            "+": ["4","20","29","33"],
+            "-": ["5","21","30","34"],
+            "*": ["6","7","8","22","31","35","36","40"],
+            ":": ["11","23","32","41"],
+            "%": ["12","24","42"],
+            "?": ["1"],
+            " ": ["13","14","43","10"]
+        }
+        self.trio_arg_funcs = ["9"]
+
+        # Создание макета на основе функции
+        if func_number in self.single_arg_funcs:
+            self.single_input_entry = tk.Entry(content_frame, width=60)
+            self.single_input_entry.pack(pady=5)
+        elif func_number in self.trio_arg_funcs:
+            self.create_trio_input_frame(content_frame)
+        else:
+            operator = self.get_operator(func_number)
+            if operator:
+                self.create_dual_input_frame(content_frame, operator)
+
+        # Лейбл для вывода ошибок валидации
+        self.error_label = tk.Label(content_frame, text="", fg="red")
+        self.error_label.pack(pady=(0, 10))
+
+        # Кнопка для выполнения функции
+        self.run_button = tk.Button(content_frame, text="Выполнить",
+                                    command=lambda: self.run_function(func_number, result_label))
+        self.run_button.pack(pady=10)
+
+        # Лейбл для отображения результата
+        self.result_label = tk.Label(content_frame, text="", font=("Arial", 18), fg="#D3D3D3")
+        self.result_label.pack(pady=5)
+
+
+    def get_operator(self, func_number):
+        """Определяет оператор для двух аргументов на основе номера функции."""
+        for operator, func_list in self.dual_arg_funcs.items():
+            if func_number in func_list:
+                return operator
+        return None
+
+    def create_dual_input_frame(self, parent, operator): #Создает макет с двумя полями ввода и оператором между ними.
+        dual_input_frame = tk.Frame(parent)
+        dual_input_frame.pack(pady=5)
+
+        self.first_input_entry = tk.Entry(dual_input_frame, width=24)
+        self.first_input_entry.pack(side="left", padx=5)
+
+        self.operator_label = tk.Label(dual_input_frame, text=operator, font=("Arial", 12))
+        self.operator_label.pack(side="left", padx=5)
+
+        self.second_input_entry = tk.Entry(dual_input_frame, width=24)
+        self.second_input_entry.pack(side="left", padx=5)
+
+
+    def create_trio_input_frame(self, parent):
+        trio_input_frame = tk.Frame(parent)
+        trio_input_frame.pack(pady=5)
+
+        self.first_input_entry_in_trio = tk.Entry(trio_input_frame, width=24)
+        self.first_input_entry_in_trio.pack(side="left", padx=5)
+
+        tk.Label(trio_input_frame, text="-", font=("Arial", 12)).pack(side="left", padx=5)
+
+        self.second_input_entry_in_trio = tk.Entry(trio_input_frame, width=24)
+        self.second_input_entry_in_trio.pack(side="left", padx=5)
+
+        tk.Label(trio_input_frame, text="*", font=("Arial", 12)).pack(side="left", padx=5)
+
+        self.tree_input_entry_in_trio = tk.Entry(trio_input_frame, width=24)
+        self.tree_input_entry_in_trio.pack(side="left", padx=5)
+
+    def get_inputs(self, func_number):
+        if func_number in self.single_arg_funcs:
+            return [self.single_input_entry.get()]
+        elif func_number in self.trio_arg_funcs:
+            return [
+                self.first_input_entry_in_trio.get(),
+                self.second_input_entry_in_trio.get(),
+                self.tree_input_entry_in_trio.get(),
+            ]
+        return [self.first_input_entry.get(), self.second_input_entry.get()]
+
+    def run_function(self, func_number, result_label):
+        """Выполняет функцию в зависимости от макета и отображает результат."""
+        args = self.get_inputs(func_number)
+        validation_error = self.validate_input(args)
+        if validation_error:
+            self.error_label.config(text=validation_error)
+            self.error_label.config(text="")  # Очистка поля результата
+            return
+        if 0 <= int(func_number) <= 14: #N
+            for arg in args:
+                if not arg.isdigit() or int(arg) < 0:
+                    self.result_label.config(text="")
+                    self.error_label.config(text="")
+                    self.error_label.config(text="Ошибка: Надо ввести натуральное число.")
+                    return
+            if int(func_number) == 6:
+                if 0 <= int(args[1]) <= 9:
+                    args = [NaturalNumber(arg) for arg in args]
+                else:
+                    self.result_label.config(text="")
+                    self.error_label.config(text="")
+                    self.error_label.config(text="Ошибка:во втором поле должна быть введена одна цифра (0-9).")
+            else:
+                self.result_label.config(text="")
+                args = [NaturalNumber(arg) for arg in args]
+
+        elif 15 <= int(func_number) <= 24:  # Z
+            if int(func_number) == 18:
+                args = [NaturalNumber(arg) for arg in args]
+            for arg in args:
+                if not (arg.lstrip('-').isdigit()):
+                    self.result_label.config(text="")
+                    self.error_label.config(text="")
+                    self.error_label.config(text="Ошибка: Надо ввести целое число.")
+                    return
+            else:
+                args = [IntegerNumber(arg) for arg in args]
+
+        elif 25 <= int(func_number) <= 32: #Q
+            if int(func_number) == 27:
+                args = [IntegerNumber(arg) for arg in args]
+            else:
+                res = []
+                for i in range(len(args)):
+                    if args[i].count("/") == 0:
+                        self.result_label.config(text="")
+                        self.error_label.config(text="")
+                        self.error_label.config(text="Ошибка: Надо ввести рациональное число.")
+                        return
+                    args[i] = args[i].split("/")
+                    if args[i][1] == "0":
+                        self.result_label.config(text="")
+                        self.error_label.config(text="")
+                        self.error_label.config(text="Знаменатель не может быть нулём")
+                    res.append(RationalNumber(IntegerNumber(args[i][0]), NaturalNumber(args[i][1])))
+                args = res
+
+        elif 32 <= int(func_number) <= 45: #P
+            res = []
+            if int(func_number) == 35:
+                res.append(makePolynomial(args[0]))
+                rational_parts = args[1].split("/")
+                if str(NaturalNumber(rational_parts[1])) == "0":
+                    self.result_label.config(text="")
+                    self.error_label.config(text="")
+                    self.error_label.config(text="Знаменатель не может быть нулём")
+                res.append(RationalNumber(IntegerNumber(rational_parts[0]), NaturalNumber(rational_parts[1])))
+                args = res
+            elif int(func_number) == 36:
+                res.append(makePolynomial(args[0]))
+                res.append(NaturalNumber(args[1]))
+                args = res
+            else:
+                for i in range(len(args)):
+                    res.append(makePolynomial(args[i]))
+                args = res
+        function = functions_dict.get(func_number)
+
+        if function:
+            validation_error = self.validate_input(args)
+            if validation_error:
+                self.error_label.config(text=validation_error)
+                self.result_label.config(text="")  # Очистка поля результата
+            else:
+                self.error_label.config(text="")  # Очистка ошибок
+                try:
+                    result_text = function(*args)  # Выполнение функции с аргументами
+                    result_text = makePolynomial(str(result_text)).getNiceStr()
+                    self.result_label.config(text=result_text)  # Отображение результата
+                except Exception as e:
+                    self.error_label.config(text=f"Ошибка выполнения: {e}")
+
+    def validate_input(self, args):
+        """Валидация данных, введённых пользователем, перед отправкой в функцию."""
+        if len(args) == 1:
+            if not str(args)[0].strip():
+                return "Ошибка: введите значение."
+        elif len(args) == 2:
+            if not str(args)[0].strip() or not str(args)[1].strip():
+                return "Ошибка: оба поля должны быть заполнены."
+        elif len(args) == 3:
+            if not str(args)[0].strip() or not str(args)[1].strip() or not str(args)[2].strip():
+                return "Ошибка: все три поля должны быть заполнены."
+        return None  # Нет ошибок
